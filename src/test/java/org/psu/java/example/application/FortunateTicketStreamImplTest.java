91713e309427ad06ba60584a735363ded2f8c632
vrusakov <rusakov_vas@mail.ru>
2023-11-18 17:19:06 +0500

    Добавлена в FortunateTicketStreamImpl проверка состояния spy + настроен Slf4j

--- src/test/java/org/psu/java/example/application/FortunateTicketStreamImplTest.java
+++ src/test/java/org/psu/java/example/application/FortunateTicketStreamImplTest.java
@@ -10,33 +10,34 @@ import org.mockito.Mockito;
 import org.psu.java.example.domain.Ticket;
 import org.psu.java.example.infrastructure.TicketGenerator;
 import org.psu.java.example.infrastructure.TicketImpl;
-import org.psu.java.example.infrastructure.TicketRecordImpl;
 
 import java.util.Iterator;
 import java.util.stream.IntStream;
 
 import static org.junit.Assert.*;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 /**
  * Тесты для {@link FortunateTicketStreamImpl}
  */
+@Slf4j
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class FortunateTicketStreamImplTest {
 
     TicketGenerator ticketGenerator;
     int maxNumber;
     FortunateTicketService service;
+    Ticket mockTicket;
 
     @Before
     public void setUp() {
         maxNumber = (int) Math.pow(10, 4);
+        mockTicket = Mockito.spy(new TicketImpl(4, 0));
+        when(mockTicket.isFortunate()).thenReturn(true);
         Iterator<Ticket> mockIterator = IntStream
                 .range(0, maxNumber)
                 .mapToObj(number -> {
-                    Ticket mockTicket = Mockito.spy(new TicketImpl(4, number));
-                    when(mockTicket.isFortunate()).thenReturn(true);
-                    System.out.println(mockTicket.toString());
+                    log.info(String.format("Формируем билет №%d", number));
                     return mockTicket;
                 })
                 .iterator();
@@ -60,6 +61,7 @@ public class FortunateTicketStreamImplTest {
 
         // then
         assertEquals("Должно быть 10 000 счастливых билетов", maxNumber, actual);
+        Mockito.verify(mockTicket, atLeast(maxNumber)).isFortunate();
     }
 
 }
